4b517e6cc1196784917c2024ec5b3d1b925c66f2
trzynasciegroszy <topdrive940@gmail.com>
2021-02-25 20:05:13 +0000

    Basic homepage

---
+++ src/components/BottomBar.vue
@@ -0,0 +1,38 @@
+<template lang="">
+    <div>
+<img src="../assets/winlogo.png">
+<form action="https://www.google.com/search"  method="get"  target="_blank">
+<input  name="q" placeholder="Search in google" type="text">
+</form>
+ </div>
+</template>
+<script>
+export default {
+  name: "BottomBar",
+  components: {},
+};
+
+</script>
+<style scoped>
+div {
+  height: 35px;
+  position: fixed;
+  top: 94%;
+  position: bottom;
+  background-color: black;
+  width: 100%;
+  margin-top: 12px;
+  padding: 0;
+}
+input {
+  posiotion: absolute;
+  margin-left: 40px;
+  height: 28px;
+  width: 250px;
+}
+img {
+  position: absolute;
+  width: 30px;
+  height: 30px;
+}
+</style>
---
+++ src/components/Homepage.vue
@@ -0,0 +1,21 @@
+<template lang="">
+    <div id="Homepage">
+<img src="../assets/homepage.jpg">
+    </div>
+</template>
+<script>
+export default {
+  name: "Homepage",
+  components: {},
+};
+</script>
+<style scoped>
+img {
+  border: 2px solid#ddd;
+  border-radius: 10px;
+  display: block;
+  margin: auto;
+  padding: 0;
+  width: 49%;
+}
+</style>
