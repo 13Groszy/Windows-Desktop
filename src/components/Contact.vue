54ee3c73bd8e6eeb396ae3f65fb6591efc8abcb6
trzynasciegroszy <topdrive940@gmail.com>
2021-02-27 15:30:40 +0000

    Menu added

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="Contact">
-    <img src="../assets/mailbox.jpg" @click="skill = 1" />
-    <div v-if="this.skill != 0">
-      <img class="modal" src="../assets/modal2.jpeg" @click="skill--" />
+    <img src="../assets/mailbox.jpg" @click="contact = 1" />
+    <p>Contact Me</p>
+    <div v-if="this.contact != 0">
+      <img class="modal" src="../assets/modal2.jpeg" @click="contact--" />
     </div>
   </div>
 </template>
@@ -12,26 +13,21 @@ export default {
   components: {},
   data: function () {
     return {
-      skill: 0,
+      contact: 0,
     };
   },
 };
 </script>
 <style scoped>
-body {
-  position: absolute;
-}
 img {
   height: 70px;
   width: 70px;
   bottom: 80%;
-  left: 10px;
+  left: 20px;
+  position: relative;
 }
 img:hover {
-  height: 75px;
-  width: 75px;
-  left: 10px;
-  bottom: 79.5%;
+  opacity: 0.8;
 }
 .modal {
   width: 50%;
@@ -40,11 +36,21 @@ img:hover {
   left: 10%;
   border: 2px solid #f0f8ff;
   border-radius: 50px;
+  position: fixed;
 }
 .modal:hover {
   width: 51%;
   height: 51%;
   bottom: 24%;
   left: 9.5%;
+  opacity: 1;
+}
+p {
+  color: white;
+  font-weight: bold;
+  font-size: 20px;
+  left: 7px;
+  top: -20px;
+  position: relative;
 }
 </style>
